c35452aa0213dbfa12f821c10506237630b8f48b
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2021-08-03 09:41:45 +0530

    miatoll: Improve post_boot tuning for performance
    * kang cpusets and schedutil ratelimits from sunfish
     * enable schedtune boost and prefer_idle
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>
    Signed-off-by: ihsanulrahman <ihsanulrahman@gmail.com>

--- rootdir/bin/init.qcom.post_boot-atoll.sh
+++ rootdir/bin/init.qcom.post_boot-atoll.sh
@@ -79,11 +79,15 @@ echo 740000 > /proc/sys/kernel/sched_little_cluster_coloc_fmin_khz
 echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 500 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
 echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+echo 500 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
 echo 1248000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
 echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
 # configure governor settings for big cluster
 echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
+echo 500 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
+echo 20000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/down_rate_limit_us
 echo 500 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
 echo 20000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/down_rate_limit_us
 echo 1267200 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
@@ -182,6 +186,15 @@ echo 0-5,7 > /dev/cpuset/foreground/cpus
 echo 4-5 > /dev/cpuset/background/cpus
 echo 2-5 > /dev/cpuset/system-background/cpus
 echo 2-5 > /dev/cpuset/restricted/cpus
+
+# EAS
+echo 95 > /proc/sys/kernel/sched_upmigrate
+echo 85 > /proc/sys/kernel/sched_downmigrate
+
+# Enable schedutil predicted-load boosting
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/pl
+echo 1 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/pl
+
 
 # schedtune parameters
 echo 1 > /dev/stune/camera-daemon/schedtune.prefer_idle
