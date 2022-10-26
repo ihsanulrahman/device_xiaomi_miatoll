2c09db7dea06888098ae0da5543ced5235a3c50a
Lucchetto <lucchetto.tie@live.com>
2022-10-26 21:43:18 +0530

    miatoll: Disable phantom process monitoring
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ TARGET_BOARD_PLATFORM := atoll
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
