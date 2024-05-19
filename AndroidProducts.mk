bcc2646e612d6a9437d916b65409092ae371f81f
Michael Bestas <mkbestas@lineageos.org>
2024-05-20 01:45:16 +0300

    miatoll: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>
    Signed-off-by: ihsanulrahman <ihsanulrahman@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,3 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/voltage_miatoll.mk
-
-COMMON_LUNCH_CHOICES := \
-    voltage_miatoll-ap1a-user \
-    voltage_miatoll-ap1a-userdebug \
-    voltage_miatoll-ap1a-eng
