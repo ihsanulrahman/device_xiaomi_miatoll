1fc41775edac1ab096bfe9a6cbaa6203dd9ea6f2
clarencelol <clarencekuiek@proton.me>
2024-06-02 22:29:02 +0800

    miatoll: Use full camera provider blobs
    * MiuiCamera glory
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- device.mk
+++ device.mk
@@ -65,11 +65,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor
-
-PRODUCT_PACKAGES += \
     libcamera_provider_shim \
     libMegviiFacepp-0.5.2 \
     libmegface \
