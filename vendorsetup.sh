3ea2b5bb024596487d4a3a27f24a749c3be4974e
ihsanulrahman <ihsanulrahman@gmail.com>
2024-08-16 21:49:14 +0500

    miatoll: vendorsetup: update repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ end="\033[0m"
 
 # Vendor & Kernel Sources
 echo -e "${color}Cloning vendor${end}"
-git clone --depth=1 -b udc https://github.com/ihsanulrahman/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
+git clone --depth=1 -b icecream https://github.com/ihsanulrahman/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
 
 echo -e "${color}Cloning kernel${end}"
 git clone --depth=1 -b universe https://github.com/BlissRoms-Devices/android_kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
@@ -18,11 +18,12 @@ git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage
 
 sleep 1
 
-# Miui Camera & OP Dolby 
+# Miui Camera & Dolby 
 echo -e "${color}Cloning Miui Camera${end}"
-git clone https://github.com/ihsanulrahman/vendor_xiaomi_miuicamera --depth=1 vendor/xiaomi/miuicamera
+git clone -b 14 https://github.com/ihsanulrahman/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+bash vendor/xiaomi/miuicamera/vendorsetup.sh
 
-echo -e "${color}Cloning OP Dolby${end}"
+echo -e "${color}Cloning Dolby${end}"
 git clone https://github.com/veux-frost-testzone/hardware_dolby hardware/dolby
 
 sleep 1
