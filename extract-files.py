2bcae76f5fcde8c0f1280de62cd8d66d8627fa15
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 13:45:39 +0300

    sm6250-common: fix QCOM WFD ELF checks
    Change-Id: If3d64c1cfa8fb6d30ca29a3260d2ebe51c9ffd2f

--- extract-files.py
+++ extract-files.py
@@ -38,7 +38,9 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
         'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
