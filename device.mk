07067415fd81db80a7bd2e5f3facc67b2c0b388e
COSMIC <rayavanindra@gmail.com>
2024-09-14 22:14:34 +0530

    miatoll: Shim libwvhidl with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that libwvhidl require.
    
    Signed-off-by: ihsanulrahman <ihsanulrahman@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
 
+PRODUCT_PACKAGES += \
+    libcrypto_shim
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
