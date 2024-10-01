2d90666a1e48a311e6b6de75919702f2f3c1260d
IHSAN <ihsanulrahman@gmail.com>
2024-10-02 00:12:02 +0500

    miatoll: switch to KINESIS Kernel
    Signed-off-by: IHSAN <ihsanulrahman@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,7 +104,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump
-TARGET_KERNEL_CONFIG := cust_defconfig
+TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 KERNEL_CC := CC=clang
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ echo -e "${color}Cloning vendor${end}"
 git clone --depth=1 -b udc https://github.com/ihsanulrahman/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
 
 echo -e "${color}Cloning kernel${end}"
-git clone --depth=1 -b universe https://github.com/BlissRoms-Devices/android_kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
+git clone --depth=1 -b 15 https://github.com/clarencekopitiam/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
 
 sleep 1
 
