679d0dcc06bbcb1fcb56fd9aea985fcaed6d603f
Michael Bestas <mkbestas@lineageos.org>
2024-10-19 01:06:34 +0800

    miatoll: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: IHSAN <ihsanulrahman@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020, 2022 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -17,6 +16,10 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
 
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
+
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
@@ -27,24 +30,29 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
+ONLY_FIRMWARE=
 KANG=
 SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -56,10 +64,12 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/etc/camera/camxoverridesettings.txt)
+            [ "$2" = "" ] && return 0
             sed -i "s/0x10082/0/g" "${2}"
             sed -i "s/0x1F/0x0/g" "${2}"
             ;;
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
+            [ "$2" = "" ] && return 0
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;
         vendor/etc/seccomp_policy/atfwd@2.0.policy)
@@ -67,9 +77,11 @@ function blob_fixup() {
             echo 'gettid: 1' >> ${2}
             ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
+            [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         vendor/lib64/hw/fingerprint.fpc.default.so)
+            [ "$2" = "" ] && return 0
             # NOP out report_input_event()
             "${SIGSCAN}" -p "30 00 00 90 11 3a 42 f9" -P "30 00 00 90 1f 20 03 d5" -f "${2}"
             ;;
@@ -78,18 +90,28 @@ function blob_fixup() {
             "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
         system_ext/etc/init/wfdservice.rc)
+            [ "$2" = "" ] && return 0
             sed -i "/^service/! s/wfdservice$/wfdservice64/g" "${2}"
             ;;
         # Remove dependency on android.hidl.base@1.0 for WFD native library.
         system_ext/lib64/libwfdnative.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+if [ -z "${ONLY_FIRMWARE}" ]; then
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
 "${MY_DIR}/setup-makefiles.sh"
