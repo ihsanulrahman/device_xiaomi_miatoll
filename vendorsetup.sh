d0244045f7a2616320e09ce11546d021963cf292
IHSAN <ihsanulrahman@gmail.com>
2024-10-19 23:23:03 +0500

    miatoll: update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ end="\033[0m"
 
 # Vendor & Kernel Sources
 echo -e "${color}Cloning vendor${end}"
-git clone --depth=1 -b icecream https://github.com/ihsanulrahman/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
+git clone --depth=1 -b elf-test https://github.com/ihsanulrahman/vendor_xiaomi_miatoll vendor/xiaomi/miatoll
 
 echo -e "${color}Cloning kernel${end}"
 git clone --depth=1 -b 15 https://github.com/clarencekopitiam/kernel_xiaomi_sm6250 kernel/xiaomi/sm6250
@@ -20,7 +20,7 @@ sleep 1
 
 # Miui Camera & Dolby 
 echo -e "${color}Cloning Miui Camera${end}"
-git clone -b 14 https://github.com/ihsanulrahman/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+git clone -b 15 https://github.com/ihsanulrahman/vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 bash vendor/xiaomi/miuicamera/vendorsetup.sh
 
 echo -e "${color}Cloning Dolby${end}"
