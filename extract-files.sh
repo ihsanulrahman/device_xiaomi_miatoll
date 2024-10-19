9854b5c78cb2b5d46cd16c398e29d30c0eea8c74
Bruno Martins <bgcngm@gmail.com>
2024-10-19 14:57:51 +0800

    miatoll: Use prebuilt vendor.qti.hardware.camera.device@1.0 differently
    Fixes:
    error: vendor/xiaomi/miatoll/Android.bp:8100:1: module "prebuilt_vendor.qti.hardware.camera.device@1.0" variant "android_vendor_arm64_armv8-2a-dotprod_cortex-a76_shared": partition is different: system_ext(vendor.qti.hardware.camera.devi
    ce@1.0) != vendor(prebuilt_vendor.qti.hardware.camera.device@1.0)
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -91,6 +91,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
+        vendor/lib64/android.hardware.camera.provider@2.4-legacy.so | vendor/bin/hw/android.hardware.camera.provider@2.4-service_64 | vendor/lib64/camera.device@1.0-impl.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.camera.device@1.0.so" "vendor.qti.hardware.camera.device@1.0_vendor.so" "${2}"
+            ;;
         system_ext/etc/init/wfdservice.rc)
             [ "$2" = "" ] && return 0
             sed -i "/^service/! s/wfdservice$/wfdservice64/g" "${2}"
